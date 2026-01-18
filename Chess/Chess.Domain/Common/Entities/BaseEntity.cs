c9ceb620cc5a5e69d18c16053955348c15d240be
Talha Ahmad <talha.ahmad.tech@gmail.com>
2026-01-18 16:53:19 +0500

    Setup base entity

---
+++ Chess/Chess.Domain/Common/Entities/BaseEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace Chess.Domain.Common.Entities
+{
+    public abstract class BaseEntity
+    {
+        // Attribute
+        public int Id { get; protected set; }
+    }
+}
---
+++ Chess/Chess.Domain/Player/Entities/Player.cs
@@ -0,0 +1,10 @@
+﻿using Chess.Domain.Common.Entities;
+
+namespace Chess.Domain.Player.Entities
+{
+    public sealed class Player : AuditableEntity
+    {
+        // Attributes
+        public string Name { get; private set; }
+    }
+}
